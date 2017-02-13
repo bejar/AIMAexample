26501a343724985c347204a4bba9ea5ae18037cc
javier.bejar <bejar@cs.upc.edu>
2017-02-13 07:47:33 +0100

    some fixing

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ import aima.search.framework.Problem;
 import aima.search.framework.Search;
 import aima.search.framework.SearchAgent;
 import aima.search.informed.AStarSearch;
+import aima.search.informed.IterativeDeepeningAStarSearch;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
